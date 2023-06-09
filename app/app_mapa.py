39638015c4df91af0a4eb346d616a6dcbefdee49
Fernando Martinez <46614852+zenitrems@users.noreply.github.com>
2023-06-08 22:26:55 -0500

    ident mapa app
    historico añadido

--- app/app_mapa.py
+++ app/app_mapa.py
@@ -1,54 +1,145 @@
+"""
+Sismos Get 
+"""
+
 import os
-import folium
-import folium.plugins as plugins
 import concurrent.futures
+from datetime import datetime
+from dotenv import load_dotenv
+import folium
+from folium import plugins
 from pymongo.errors import PyMongoError
-from datetime import datetime, timedelta
 from flask import Flask, render_template
 from get_mongo_db import mongodb
-from dotenv import load_dotenv
 
 
 load_dotenv()
 app = Flask(__name__)
 dbname = mongodb()
 
-
 mexico_bounds = [[14.5, -120.9], [32.7, -85]]
+mexico_center = [19.4, -99.1]
+SSN_REF = '<a href=\"http://www.ssn.unam.mx/\">Servicio Sismógico Nacional</a>'
+UNAM_REF = '<a href=\"https://www.unam.mx/\">UNAM</a>'
+MAP_ATRIBUTION = f"{SSN_REF}|{UNAM_REF}"
 
 
 def get_marker_color(magnitud):
-    if magnitud == 0:
-        return '#fff'
-    elif magnitud < 3.0:
-        return '#62ab5d'
+    """asigna color por magnitud"""
+    color = '#fff'
+    if magnitud < 3.0:
+        color = '#62ab5d'
     elif magnitud < 4.0:
-        return '#ffc304'
+        color = '#ffc304'
     elif magnitud < 5.0:
-        return '#f79b3f'
+        color = '#f79b3f'
     elif magnitud < 6.0:
-        return '#ea5354'
+        color = '#ea5354'
     elif magnitud < 7.0:
-        return '#a85164'
+        color = '#a85164'
     elif magnitud < 8.0:
-        return '#ff3fff'
+        color = '#ff3fff'
     else:
-        return '#9f3f9f'
+        color = '#9f3f9f'
+    return color
 
 
-@app.route('/')
-def index(start_date=None):
-
+def draw_points(points_data):
+    """Marker Maker for points"""
     mapa = folium.Map(
+        location=mexico_center,
         tiles=os.getenv('MAP_TILE'),
-        attr='<a href=\"http://www.ssn.unam.mx/\">Servicio Sismógico Nacional</a>|<a href=\"https://www.unam.mx/\">UNAM</a>',
-        zoom_start=8,
-        min_zoom=4,
+        attr=MAP_ATRIBUTION,
+        min_zoom=5,
         zoom_control=False,
         control_scale=True,
         max_bounds=True
     )
+    ultimo_sismo = points_data[-1]
+    lat_ultimo_sismo = ultimo_sismo['latitud']
+    lon_ultimo_sismo = ultimo_sismo['longitud']
+
+    # Agregar marcador de ultimo sismo
+    magnitud_ultimo_sismo = ultimo_sismo['magnitud']
+    color_magnitud = get_marker_color(magnitud_ultimo_sismo)
+    # Template popup ultimo sismo
+    popup_ultimo_sismo = f"""
+       <p>ultimo sismo</p>
+       <h4>{ultimo_sismo['referencia']}</h4>
+       <strong style="color:{color_magnitud}">M </strong><strong style="color:{color_magnitud}">{magnitud_ultimo_sismo}</strong><br>
+       <span>Profundidad: </span>{ultimo_sismo['profundidad']}<span> Kilometros</span></br>
+       <strong>{ultimo_sismo['fecha']}, {ultimo_sismo['hora']}</strong>
+       """
+    marker_ultimo_sismo = folium.Marker(
+        location=[lat_ultimo_sismo, lon_ultimo_sismo],
+        icon=plugins.BeautifyIcon(
+            icon_shape='circle',
+            border_width=1,
+            background_color=get_marker_color(magnitud_ultimo_sismo),
+            inner_icon_style='font-size:8px; padding: 4px; text-align:center;',
+            number=magnitud_ultimo_sismo
+        )
+    )
+    for point in points_data:
+        lat = point['latitud']
+        lon = point['longitud']
+        referencia = point['referencia']
+        magnitud = point['magnitud']
+        color_magnitud = get_marker_color(magnitud)
+        profundidad = point['profundidad']
+        fecha = point['fecha']
+        hora = point['hora']
+        # Template popup epicentro
+
+        popup_content = f"""
+            <h4>{referencia}</h4>
+            <strong style="color:{color_magnitud}">M </strong><strong style="color:{color_magnitud}">{magnitud}</strong></br>
+            <span>Profundidad: </span>{profundidad}<span> Km</span><br>
+            <strong>{fecha}, {hora}</strong>
+            """
+        sismo_marker = folium.Marker(
+            location=[lat, lon],
+            icon=plugins.BeautifyIcon(
+                icon_shape='doughnut',
+                border_width=1,
+                background_color=color_magnitud,
+            )
+        )
+        sismo_marker.add_child(folium.Tooltip(text=popup_content))
+        sismo_marker.add_to(mapa)
+
+    listener_script = """
+    <script>
+        window.addEventListener('message', function (event) {
+          if (event.data.type === 'center') {
+            var lat = event.data.lat;
+            var lon = event.data.lon;
+            parent.postMessage({ type: 'center', lat: lat, lon: lon }, '*');
+            console.log(lat, lon)
+            var mapElement = document.querySelector('.folium-map');
+            if (mapElement) {
+              var mapObject = mapElement._leaflet_map;
+              document.addEventListener('DOMContentLoaded', function() {
+                var bounds = L.latLngBounds([[lat, lon], [lat, lon]]);
+                mapObject.fitBounds(bounds);
+              });
+            }
+          }
+        });
+    </script>
+    """
+    marker_ultimo_sismo.add_child(folium.Tooltip(text=popup_ultimo_sismo))
+    marker_ultimo_sismo.add_to(mapa)
+
+    # Guardar el mapa en un archivo HTML
     mapa.fit_bounds(mexico_bounds)
+    mapa.get_root().html.add_child(folium.Element(listener_script))
+    mapa.save('static/mapa.html')
+
+
+@app.route('/')
+def index(start_date=None):
+    """App Index"""
 
     # Establecer fecha actual si no se encontro solicitud de fecha
     if start_date is None:
@@ -62,194 +153,44 @@ def index(start_date=None):
         data_collection = list(collection_name.find(
             {"fecha": start_date_str}))
 
-    except PyMongoError as e:
-        print("error mongo", str(e))
+    except PyMongoError as mongo_error:
+        print("error mongo", str(mongo_error))
 
     if data_collection:
-        ultimo_sismo = data_collection[-1]
-        lat_ultimo_sismo = ultimo_sismo['latitud']
-        lon_ultimo_sismo = ultimo_sismo['longitud']
-
-        # Agregar un marcador al mapa para cada epicentro
-
-        for registro in data_collection:
-            lat = registro['latitud']
-            lon = registro['longitud']
-            magnitud = registro['magnitud']
-
-            color_magnitud = get_marker_color(magnitud)
-
-            # Template popup epicentro
-            popup_content = f"""
-                <h4>{registro['referencia']}</h4>
-                <strong style="color:{color_magnitud}">M </strong><strong style="color:{color_magnitud}">{magnitud}</strong></br>
-                <span>Profundidad: </span>{registro['profundidad']}<span> Km</span><br>
-                <strong>{registro['fecha']}, {registro['hora']}</strong>
-                """
-            sismo_marker = folium.Marker(
-                location=[lat, lon],
-                icon=plugins.BeautifyIcon(
-                    icon_shape='doughnut',
-                    border_width=1,
-                    background_color=color_magnitud,
-                )
-            )
-            sismo_marker.add_child(folium.Tooltip(text=popup_content))
-            sismo_marker.add_to(mapa)
-
-        # Agregar marcador de ultimo sismo
-        magnitud_ultimo_sismo = ultimo_sismo['magnitud']
-
-        # Template popup ultimo sismo
-        popup_ultimo_sismo = f"""
-            <p>ultimo sismo</p>
-            <h4>{ultimo_sismo['referencia']}</h4>
-            <strong style="color:{color_magnitud}">M </strong><strong style="color:{color_magnitud}">{magnitud_ultimo_sismo}</strong><br>
-            <span>Profundidad: </span>{ultimo_sismo['profundidad']}<span> Kilometros</span></br>
-            <strong>{ultimo_sismo['fecha']}, {ultimo_sismo['hora']}</strong>
-            """
-        marker_ultimo_sismo = folium.Marker(
-            location=[lat_ultimo_sismo, lon_ultimo_sismo],
-            icon=plugins.BeautifyIcon(
-                icon_shape='circle',
-                border_width=1,
-                background_color=get_marker_color(magnitud_ultimo_sismo),
-                inner_icon_style='font-size:8px; padding: 4px; text-align:center;',
-                number=magnitud_ultimo_sismo
-            )
-        )
-        marker_ultimo_sismo.add_child(folium.Tooltip(text=popup_ultimo_sismo))
-        marker_ultimo_sismo.add_to(mapa)
-
-        listener_script = """
-        <script>
-            window.addEventListener('message', function (event) {
-              if (event.data.type === 'center') {
-                var lat = event.data.lat;
-                var lon = event.data.lon;
-                parent.postMessage({ type: 'center', lat: lat, lon: lon }, '*');
-                console.log(lat, lon)
-                var mapElement = document.querySelector('.folium-map');
-                if (mapElement) {
-                  var mapObject = mapElement._leaflet_map;
-                  document.addEventListener('DOMContentLoaded', function() {
-                    var bounds = L.latLngBounds([[lat, lon], [lat, lon]]);
-                    mapObject.fitBounds(bounds);
-                  });
-                }
-              }
-            });
-        </script>
-        """
-
-        # Guardar el mapa en un archivo HTML
-        mapa.get_root().html.add_child(folium.Element(listener_script))
-        mapa.save('static/mapa.html')
-        selected_event = None
-        return render_template('index.html', eventos=data_collection, selected_evento=selected_event)
-
-    else:
-        oneday = datetime.now() - timedelta(days=1)
-        oneday_str = oneday.strftime("%Y-%m-%d")
-        try:
-            data_collection = list(collection_name.find(
-                {"fecha": oneday_str}))
-        except PyMongoError as e:
-            print("error mongo", str(e))
-
-        # Agregar un marcador al mapa para cada epicentro
-        for registro in data_collection:
-            lat = registro['latitud']
-            lon = registro['longitud']
-            magnitud = registro['magnitud']
-
-            color_magnitud = get_marker_color(magnitud)
-
-            # Template popup epicentro
-            popup_content = f"""
-                <h4>{registro['referencia']}</h4>
-                <strong style="color:{color_magnitud}">M </strong><strong style="color:{color_magnitud}">{magnitud}</strong></br>
-                <span>Profundidad: </span>{registro['profundidad']}<span> Km</span><br>
-                <strong>{registro['fecha']}, {registro['hora']}</strong>
-                """
-            sismo_marker = folium.Marker(
-                location=[lat, lon],
-                icon=plugins.BeautifyIcon(
-                    icon_shape='doughnut',
-                    border_width=1,
-                    background_color=color_magnitud,
-                )
-            )
-            sismo_marker.add_child(folium.Tooltip(text=popup_content))
-            sismo_marker.add_to(mapa)
-
-        mapa.save('static/mapa.html')
-        return render_template('index.html', eventos=data_collection)
+        draw_points(data_collection)
+    return render_template('index.html', eventos=data_collection)
 
 
 # Search Start date
 @app.route('/date/<start_date>')
 def index_with_date(start_date):
+    """Index for date"""
     start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
     return index(start_date)
 
 
 @app.route('/historico')
 def historico():
-    mapa = folium.Map(
-        tiles=os.getenv('MAP_TILE'),
-        attr='<a href=\"http://www.ssn.unam.mx/\">Servicio Sismógico Nacional</a>|<a href=\"https://www.unam.mx/\">UNAM</a>',
-        zoom_start=8,
-        min_zoom=4,
-        zoom_control=False,
-        control_scale=True,
-        max_bounds=True
-    )
-    mapa.fit_bounds(mexico_bounds)
+    """Major Earthquakes"""
     try:
         collection_name = dbname["magAggregate"]
         data_collection = list(collection_name.find())
-    except PyMongoError as mongoError:
-        print("mongoError", str(mongoError))
+    except PyMongoError as mongo_error:
+        print("mongoError", str(mongo_error))
 
     if data_collection:
-        for point in data_collection:
-            lat = point['latitud']
-            lon = point['longitud']
-            mag = point['magnitud']
-            color_magnitud = get_marker_color(mag)
-            # Template popup epicentro
-            popup_content = f"""
-                <h4>{point['referencia']}</h4>
-                <strong style="color:{color_magnitud}">M </strong><strong style="color:{color_magnitud}">{mag}</strong></br>
-                <span>Profundidad: </span>{point['profundidad']}<span> Km</span><br>
-                <strong>{point['fecha']}, {point['hora']}</strong>
-                """
-            sismo_marker = folium.Marker(
-                location=[lat, lon],
-                icon=plugins.BeautifyIcon(
-                    icon_shape='circle',
-                    border_width=1,
-                    background_color=color_magnitud,
-                    inner_icon_style='font-size:8px;padding:4px;',
-                    number=mag
-                )
-            )
-            sismo_marker.add_child(folium.Tooltip(text=popup_content))
-            sismo_marker.add_to(mapa)
-        mapa.save('static/mapa.html')
-        return render_template('index.html', eventos=data_collection)
-    else:
-        mapa.save('static/mapa.html')
-        return render_template('index.html')
-
+        draw_points(data_collection)
+    return render_template('index.html', eventos=data_collection)
 
 # Heat Map Route
+
+
 @app.route('/heatmap')
 def heatmap():
+    """Draw Heatmap"""
     mapa = folium.Map(
         tiles=os.getenv('MAP_TILE'),
-        attr='<a href=\"http://www.ssn.unam.mx/\">Servicio Sismógico Nacional</a>|<a href=\"https://www.unam.mx/\">UNAM</a>',
+        attr=MAP_ATRIBUTION,
         zoom_start=8,
         min_zoom=4,
         zoom_control=False,
@@ -260,8 +201,8 @@ def heatmap():
     try:
         collection_name = dbname["magAggregate"]
         data_collection = list(collection_name.find())
-    except PyMongoError as mongoError:
-        print("mongoError", str(mongoError))
+    except PyMongoError as mongo_error:
+        print("mongoError", str(mongo_error))
 
     heatmap_data = []
 
@@ -285,9 +226,6 @@ def heatmap():
         )
         heat.add_to(mapa)
 
-        mapa.save('static/mapa.html')
-        return render_template('index.html')
-    else:
         mapa.save('static/mapa.html')
         return render_template('index.html')
 
