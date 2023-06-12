f4e35950540b848379590c444dca8bf06fa9d10f
Fernando Martinez <46614852+zenitrems@users.noreply.github.com>
2023-06-11 23:28:47 -0500

    Command Logger

--- get_mongo_db.py
+++ get_mongo_db.py
@@ -1,10 +1,32 @@
-from pymongo import MongoClient
+"""
+Mongo Client
+"""
+from pymongo import MongoClient, monitoring
+
+
+class CustomCommandLogger(monitoring.CommandListener):
+    """Custom"""
+
+    def started(self, event):
+        print("Comando MongoDB iniciado: %s" % (event.command_name))
+
+    def succeeded(self, event):
+        print("Comando MongoDB exitoso: %s" % (event.command_name))
+
+    def failed(self, event):
+        print("Comando MongoDB fallido: %s" % (event.command_name))
+
+
+# Configura el logger personalizado
+command_logger = CustomCommandLogger()
+
 
 def mongodb():
-    CONECTION_STRING = "mongodb://localhost"
-    client = MongoClient(CONECTION_STRING)
+    """Mongo init"""
+    client = MongoClient("mongodb://localhost",
+                         event_listeners=[command_logger])
     return client['sismicidad_SSN']
+
 
 if __name__ == "__main__":
     dbname = mongodb()
-    
