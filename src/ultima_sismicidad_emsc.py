ea7c49317514fc7276cdc5863913b269eceb014e
Fernando Martinez <46614852+zenitrems@users.noreply.github.com>
2023-06-25 18:56:16 -0500

    src:

--- src/ultima_sismicidad_emsc.py
+++ src/ultima_sismicidad_emsc.py
@@ -1,26 +1,32 @@
-
 from __future__ import unicode_literals
+import logging
+import json
+import sys
 
 from tornado.websocket import websocket_connect
 from tornado.ioloop import IOLoop
 from tornado import gen
 
-import logging
-import json
-import sys
 
-echo_uri = 'wss://www.seismicportal.eu/standing_order/websocket'
+ECHO_URI = "wss://www.seismicportal.eu/standing_order/websocket"
 PING_INTERVAL = 15
 
-#You can modify this function to run custom process on the message
+
+# You can modify this function to run custom process on the message
 def myprocessing(message):
     try:
         data = json.loads(message)
-        info = data['data']['properties']
-        info['action'] = data['action']
-        logging.info('>>>> {action:7} event from {auth:7}, unid:{unid}, T0:{time}, Mag:{mag}, Region: {flynn_region}'.format(**info))
+        info = data["data"]["properties"]
+        print(data)
+        info["action"] = data["action"]
+        logging.info(
+            ">>>> {action:7} event from {auth:7}, unid:{unid}, T0:{time}, Mag:{mag}, Region: {flynn_region}".format(
+                **info
+            )
+        )
     except Exception:
         logging.exception("Unable to parse json message")
+
 
 @gen.coroutine
 def listen(ws):
@@ -32,18 +38,20 @@ def listen(ws):
             break
         myprocessing(msg)
 
+
 @gen.coroutine
 def launch_client():
     try:
-        logging.info("Open WebSocket connection to %s", echo_uri)
-        ws = yield websocket_connect(echo_uri, ping_interval=PING_INTERVAL)
+        logging.info("Open WebSocket connection to %s", ECHO_URI)
+        ws = yield websocket_connect(ECHO_URI, ping_interval=PING_INTERVAL)
     except Exception:
         logging.exception("connection error")
     else:
         logging.info("Waiting for messages...")
         listen(ws)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     ioloop = IOLoop.instance()
     launch_client()
@@ -52,4 +60,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         logging.info("Close WebSocket")
         ioloop.stop()
-		
