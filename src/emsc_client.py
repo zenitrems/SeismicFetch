3675cba67a747dff1dec55f748e3ebd77acfc138
Fernando Martinez <46614852+zenitrems@users.noreply.github.com>
2023-06-28 20:45:07 -0500

    Web socket client

--- src/emsc_client.py
+++ src/emsc_client.py
@@ -1,6 +1,6 @@
 """
 EMSC Websocket Client
-wss://www.seismicportal.eu/standing_order/websocket
+www.seismicportal.eu
 """
 from __future__ import unicode_literals
 import json
@@ -17,52 +17,47 @@ PING_INTERVAL = 15
 
 emsc_utils = EmscUtils()
 
-class WebSocketClient:
-    """Tornado Websocket client"""
 
-    def __init__(self) -> None:
-        pass
-
-    def process_event(self, message):
-        """Process Event"""
-        try:
-            data = json.loads(message)
-            emsc_utils.process_data(data)
-        except json.JSONDecodeError:
-            logger.exception(json.JSONDecodeError)
+def process_event( message):
+    """Process Event"""
+    try:
+        data = json.loads(message)
+        emsc_utils.process_data(data)
+    except json.JSONDecodeError:
+        logger.exception(json.JSONDecodeError)
 
 
-    @gen.coroutine
-    def listen_events(self, web_socket):
-        """Listen to events"""
-        while True:
-            msg = yield web_socket.read_message()
-            if msg is None:
-                logger.info("close")
-                web_socket = None
-                break
-            self.process_event(msg)
+@gen.coroutine
+def listen_events(web_socket):
+    """Listen to events"""
+    while True:
+        msg = yield web_socket.read_message()
+        if msg is None:
+            logger.info("close")
+            web_socket = None
+            break
+        process_event(msg)
 
 
-    @gen.coroutine
-    def launch_client(self):
-        """Launch Web Socket Client"""
-        try:
-            logger.info(
-                f"Open connection to: {ECHO_URI} ping:{PING_INTERVAL}",
-            )
-            web_socket = yield websocket_connect(ECHO_URI, ping_interval=PING_INTERVAL)
-        except WebSocketError:
-            logger.exception(WebSocketError)
-        else:
-            logger.info("listening to events")
-            self.listen_events(web_socket)
+@gen.coroutine
+def launch_client():
+    """Launch Web Socket Client"""
+    try:
+        logger.info(
+            f"Open connection to: seismicportal.eu Ping:{PING_INTERVAL}",
+        )
+        web_socket = yield websocket_connect(ECHO_URI, ping_interval=PING_INTERVAL)
+    except WebSocketError:
+        logger.exception(WebSocketError)
+    else:
+        logger.info("listening to events")
+        listen_events(web_socket)
 
 
-if __name__ == "__main__":
+def main():
+    """Start Script"""
     ioloop = IOLoop.instance()
-    client = WebSocketClient()
-    client.launch_client()
+    launch_client()
     try:
         ioloop.start()
     except KeyboardInterrupt:
