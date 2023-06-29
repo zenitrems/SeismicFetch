af589f43bbd38f421f07c7bb47d5cc50e8de6a90
Fernando Martinez <46614852+zenitrems@users.noreply.github.com>
2023-06-28 19:19:30 -0500

    EMSC Websocket Client

--- src/emsc_client.py
+++ src/emsc_client.py
@@ -1,11 +1,14 @@
+"""
+EMSC Websocket Client
+wss://www.seismicportal.eu/standing_order/websocket
+"""
 from __future__ import unicode_literals
 import json
-from loguru import logger
-from tornado.websocket import websocket_connect
+from tornado.websocket import websocket_connect, WebSocketError
 from tornado.ioloop import IOLoop
 from tornado import gen
 
-from utils import EmscUtils
+from helpers import EmscUtils, logger
 
 
 ECHO_URI = "wss://www.seismicportal.eu/standing_order/websocket"
@@ -14,44 +17,52 @@ PING_INTERVAL = 15
 
 emsc_utils = EmscUtils()
 
+class WebSocketClient:
+    """Tornado Websocket client"""
 
-def myprocessing(message):
-    """Process message"""
-    try:
-        data = json.loads(message)
-        emsc_utils.process_data(data)
-    except Exception:
-        logger.exception("Unable to parse json message")
+    def __init__(self) -> None:
+        pass
 
-
-@gen.coroutine
-def listen(ws):
-    """Listen to Messages"""
-    while True:
-        msg = yield ws.read_message()
-        if msg is None:
-            logger.info("close")
-            ws = None
-            break
-        myprocessing(msg)
+    def process_event(self, message):
+        """Process Event"""
+        try:
+            data = json.loads(message)
+            emsc_utils.process_data(data)
+        except json.JSONDecodeError:
+            logger.exception(json.JSONDecodeError)
 
 
-@gen.coroutine
-def launch_client():
-    """Launch WebSocket Client"""
-    try:
-        logger.info(f"Open WebSocket connection to {ECHO_URI}", )
-        ws = yield websocket_connect(ECHO_URI, ping_interval=PING_INTERVAL)
-    except Exception:
-        logger.exception("connection error")
-    else:
-        logger.info("Waiting for messages...")
-        listen(ws)
+    @gen.coroutine
+    def listen_events(self, web_socket):
+        """Listen to events"""
+        while True:
+            msg = yield web_socket.read_message()
+            if msg is None:
+                logger.info("close")
+                web_socket = None
+                break
+            self.process_event(msg)
+
+
+    @gen.coroutine
+    def launch_client(self):
+        """Launch Web Socket Client"""
+        try:
+            logger.info(
+                f"Open connection to: {ECHO_URI} ping:{PING_INTERVAL}",
+            )
+            web_socket = yield websocket_connect(ECHO_URI, ping_interval=PING_INTERVAL)
+        except WebSocketError:
+            logger.exception(WebSocketError)
+        else:
+            logger.info("listening to events")
+            self.listen_events(web_socket)
 
 
 if __name__ == "__main__":
     ioloop = IOLoop.instance()
-    launch_client()
+    client = WebSocketClient()
+    client.launch_client()
     try:
         ioloop.start()
     except KeyboardInterrupt:
