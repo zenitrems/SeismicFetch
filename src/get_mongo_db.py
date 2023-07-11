d027c8fab39e6fc1f741005fbf7ec8f9dfb58b63
Fernando Martinez <46614852+zenitrems@users.noreply.github.com>
2023-07-11 00:12:44 -0500

    db env

--- src/get_mongo_db.py
+++ src/get_mongo_db.py
@@ -1,30 +1,17 @@
 """
 Mongo Client
 """
-from pymongo import MongoClient, monitoring
+import os
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 from helpers import logger
-
-
-class CustomCommandLogger(monitoring.CommandListener):
-    """Custom"""
-
-    def started(self, event):
-        logger.trace(f"{event.command_name} Start")
-
-    def succeeded(self, event):
-        logger.trace(f"{event.command_name} Succeeded")
-
-    def failed(self, event):
-        logger.error(f"{event.command_name} Failed")
-
-
-# Configura el logger personalizado
-command_logger = CustomCommandLogger()
 
 
 def mongodb():
     """Mongo init"""
-    client = MongoClient("mongodb://localhost", event_listeners=[command_logger])
+    try:
+        client = MongoClient(os.getenv("MONGO_URI"))
+    except PyMongoError as mongo_error:
+        logger.error("DB CONNECTION FAILED")
+        logger.exception(mongo_error)
     return client["sismicidad"]
-
-
