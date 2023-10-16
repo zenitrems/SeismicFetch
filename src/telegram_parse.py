1037645588757477de6e558dd84850b3363ff4e6
zenitrems <46614852+zenitrems@users.noreply.github.com>
2023-10-15 22:40:08 -0500

    pase event update

--- src/telegram_parse.py
+++ src/telegram_parse.py
@@ -29,20 +29,28 @@ class SsnBotParse:
                 "lat": element["geometry"]["coordinates"][1],
                 "lon": element["geometry"]["coordinates"][0],
                 "auth": element["properties"]["auth"],
-                "preliminar": element["properties"]["preliminary"],
+                "preliminary": element["properties"]["preliminary"],
             }
             self.template_event(event)
 
     def template_event(self, event):
         """Send Event to Chanel"""
+        event_location = [event["lat"], event["lon"]]
         if event["mag"] >= MAG_THRESHOLD:
-            template = (
-                f"<b>{event['auth']} | {event['magType']} {event['mag']} | Depth: {event['depth']} Km </b>\n\n"
-                f"<pre>{event['place']}</pre>\n\n"
-                f"<i>{event['time']}</i>\n\n"
-                f"<i>{event['preliminar']}</i>\n\n"
-            )
-            asyncio.run(bot.send_evento(template))
+            if event["preliminary"] == True:
+                template = (
+                    f"<b>{event['auth']} | {event['magType']} {event['mag']} (PRELIMINAR) | Depth: {event['depth']} Km </b>\n\n"
+                    f"<pre>{event['place']}</pre>\n\n"
+                    f"<i>{event['time']}</i>\n\n"
+                )
+            else:
+                template = (
+                    f"<b>{event['auth']} | {event['magType']} {event['mag']} | Depth: {event['depth']} Km </b>\n\n"
+                    f"<pre>{event['place']}</pre>\n\n"
+                    f"<i>{event['time']}</i>\n\n"
+                )
+
+            asyncio.run(bot.send_update(template, event_location))
 
 
 class UsgsBotParse:
@@ -62,20 +70,24 @@ class UsgsBotParse:
                 "depth": element["geometry"]["coordinates"][2],
                 "lat": element["geometry"]["coordinates"][1],
                 "lon": element["geometry"]["coordinates"][0],
-                "auth": "USGS",
+                "status": element["properties"]["status"],
+                "sig": element["properties"]["sig"],
+                "tsunami": element["properties"]["tsunami"],
             }
             self.template_event(event)
 
     def template_event(self, event):
         """Send Event to Chanel"""
+        event_location = [event["lat"], event["lon"]]
         if event["mag"] >= MAG_THRESHOLD:
             template = (
-                f"<b>{event['auth']} | {event['magType']} {event['mag']} | Depth: {event['depth']} Km </b>\n\n"
+                f"<b>USGS | {event['magType']} {event['mag']} | Depth: {event['depth']} Km </b>\n\n"
                 f"<pre>{event['place']}</pre>\n\n"
                 f"<i>{event['time']}</i>\n\n"
+                f"<pre>{event['status']}, SIG: {event['sig']}</pre>"
             )
 
-            asyncio.run(bot.send_evento(template))
+            asyncio.run(bot.send_update(template, event_location))
 
 
 class EmscBotParse:
@@ -101,6 +113,7 @@ class EmscBotParse:
 
     async def template_event(self, event):
         """Send Event to Chanel"""
+        event_location = [event["lat"], event["lon"]]
         if event["mag"] >= MAG_THRESHOLD:
             template = (
                 f"<b>{event['auth']} | {event['magType']} {event['mag']} | Depth: {event['depth']} Km </b>\n\n"
@@ -108,4 +121,4 @@ class EmscBotParse:
                 f"<i>{event['time']}</i>\n\n"
             )
 
-            await bot.send_evento(template)
+            await bot.send_update(template, event_location)
