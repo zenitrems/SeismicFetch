015b2291b6a648a6b571782ab052bef07ea150d3
zenitrems <46614852+zenitrems@users.noreply.github.com>
2024-07-13 18:12:59 -0500

    fetch event id & test

--- src/fetch_event_id.py
+++ src/fetch_event_id.py
@@ -4,20 +4,28 @@ EMSC Web Services
 
 import requests
 import json
+from src import helpers
 
-url = "http://www.seismicportal.eu/fdsnws/event/1/query\
-?start=2017-09-01&end=2017-11-01&format=json&minmag={minmag}".format(
-    minmag=6.5
-)
+logger = helpers.logger
+
+def fetch_by_id(main_event, source, out):
+    try:
+        url = "http://www.seismicportal.eu/eventid/api/convert?source_id={id}&source_catalog={source}&out_catalog={out}&format=json".format(id=main_event['unid'], source='UNID', out='all')
+        res = requests.get(url, timeout=15)
+        res.raise_for_status()
+        json_response = res.json()
+        json_str = json.dumps(json_response, indent=4)
+        logger.info(json_str)
+
+    except requests.Timeout as request_timeout:
+        logger.error(request_timeout)
+    except requests.ConnectionError as request_conection_error:
+        logger.error(request_conection_error)
+        
+    
+    return 
 
 
-def get_url(url):
-    res = requests.get(url, timeout=15)
-    return {"status": res.status_code, "content": res.text}
-
-
-event = get_url(url)
-print(event['content'])
-
-
-
+def event_data(response):
+    json_events = json.dumps(response, indent=4)
+    logger.info(json_events)
---
+++ test/test_fetch_event_id.py
@@ -0,0 +1,13 @@
+import unittest
+from src import fetch_event_id
+
+class TestFetchId(unittest.TestCase):
+    def setUp(self) -> None:
+        self.fetch_id = fetch_event_id.fetch_by_id
+        return super().setUp()
+    
+    def test_fetch_id(self):
+        main_event = {'unid':'20170919_0000091'}
+        source = "UNID"
+        out = 'ALL'
+        self.fetch_id(main_event, source, out)
